43e599bcad3e53c95ef5e8b2bedec0c2482308a3
Victoria-20 <victoriaocon111@gmail.com>
2023-01-06 09:55:24 +0300

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module that returns the floor of a number  """
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """ takes a float and returns the floor """
+    return math.floor(n)
