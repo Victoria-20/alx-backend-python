76f5341b793ced8cb276ee342368ff59a93d6e97
Victoria-20 <victoriaocon111@gmail.com>
2023-01-06 10:05:39 +0300

    3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module that takes a float and returns a string  """
+
+
+import math
+
+
+def to_str(n: float) -> str:
+    """ takes a float and returns the string representation """
+    return str(n)
