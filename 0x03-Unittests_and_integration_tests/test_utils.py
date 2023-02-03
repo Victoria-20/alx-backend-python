d7fbb42ce8078813b9201d9de956549f749d6a6a
Victoria-20 <victoriaocon111@gmail.com>
2023-02-03 10:13:57 +0300

    Add required files

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python3
 """0x03. Unittests and Integration Tests"""
+
+
+import unittest
+from parameterized import parameterized, expand
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand(
+        nested_map={"a": 1}, path=("a",),
+        nested_map={"a": {"b": 2}}, path=("a",),
+        nested_map={"a": {"b": 2}}, path=("a", "b")
+    )
+    def test_access_nested_map(self, nested_map, path):
+        self.assertEqual(access_nested_map(nested_map, ("license", "key")) == license_key)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
